97cbaa952527f95f1c50fc4c43d014fc5693db19
vynt2401 <nvy381932@gmai.com>
2025-08-08 20:32:51 +0700

    Auto update

--- main.py
+++ main.py
@@ -21,6 +21,12 @@ def getWeather():
     result = obj.timezone_at(lat=location.latitude, lng=location.longitude)
     timeZone.config(text=result)
 
+    long_lat.config(text = f"{round(location.latitude,4)}°N, {round(location.longitude,4)}°E")
+    
+    home = pytz.timezone(result)
+    local_time = datetime.now(home)
+    current_time = local_time.strftime("%I:%M:%p")
+    clock.config(text=current_time)
 
 
 
