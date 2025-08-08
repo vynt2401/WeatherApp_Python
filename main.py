585f16ac4a4cb96db5066efa02feed957dcf6a65
vynt2401 <nvy381932@gmai.com>
2025-08-08 20:51:28 +0700

    Auto update

--- main.py
+++ main.py
@@ -30,7 +30,7 @@ def getWeather():
     
     
     api_key = "b8c553546d209021905dd9e154c9a2a6"
-    api = f"api.openweathermap.org/data/2.5/forecast?q={city}&appid={api_key}&units=metric"
+    api = f"https://api.openweathermap.org/data/2.5/forecast?q={city}&appid={api_key}&units=metric"
     json_data = requests.get(api).json()
     
     #current weather data 
