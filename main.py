15089286a436d5d0e63e7f8a8dcd00f221fdefc6
vynt2401 <nvy381932@gmai.com>
2025-08-08 20:25:10 +0700

    Auto update

--- main.py
+++ main.py
@@ -6,7 +6,8 @@ from datetime import datetime, timedelta
 import requests
 from PIL import Image, ImageTk  
 from tkinter import messagebox
-from TimeZoneFinder import TimeZoneFinder
+from timezoneFinder
+import TimeZoneFinder
 
 
 
